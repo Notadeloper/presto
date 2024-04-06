21d2205207666ae4eec809dc844ff17d85c8297a
Edison Liang <edisonliang@Edisons-MacBook-Pro.local>
2024-04-06 17:38:40 +1100

    start

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Login } from './login.jsx'
+import { Register } from './register.jsx'
 
 function App () {
   return (
-    <>Let&apos;s go!</>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
