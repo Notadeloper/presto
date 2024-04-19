dbf3b82b10b35405106cc6f15ac1c6281e20d775
Felix Mo <felixmo@Felixs-MacBook-Air.local>
2024-04-19 16:45:54 +1000

    style for dashboard

--- frontend/src/components/newPresentationButton.jsx
+++ frontend/src/components/newPresentationButton.jsx
@@ -4,8 +4,17 @@ import NoteAddIcon from '@mui/icons-material/NoteAdd';
 
 export function NewPresentationButton ({ onClick }) {
   return (
-    <Button onClick={onClick} variant="contained" endIcon={<NoteAddIcon />} aria-label="new presentation">
-      New Presentation
-    </Button>
+    <div style={{ display: 'flex', justifyContent: 'center', margin: '20px 0 20px 0' }}>
+      <Button
+        onClick={onClick}
+        variant="contained"
+        color="secondary"
+        size="large"
+        endIcon={<NoteAddIcon />}
+        aria-label="new presentation"
+      >
+        New Presentation
+      </Button>
+    </div>
   )
 }
